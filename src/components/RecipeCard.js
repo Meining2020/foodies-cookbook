614dcb3becc1d40ff0c80df8e0bfb0d36b7f42cd
Meining <meinig.cheng@gmail.com>
2021-06-09 16:16:08 -0400

    added aria-hidden to icons, added alt for image, deleted unsed files

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import RecipeDetails from './RecipeDetails.js';
-import selfie from '../assets/notfound_placeholder.svg';
+import notFoundPhoto from '../assets/notfound_placeholder.svg';
 
-const RecipeCard = ({ recipeData, addRecipeFunction, savedRecipes, removeRecipe}) => {
+const RecipeCard = ({ recipeData, addRecipeFunction, savedRecipes, removeRecipe }) => {
 
     const { foodName, foodImg, calories, ingredientList, recipeSource, key } = recipeData;
 
@@ -32,13 +32,22 @@ const RecipeCard = ({ recipeData, addRecipeFunction, savedRecipes, removeRecipe}
         <div className="subContainer">
 
             {
-                isSaved ? <span onClick={() => { removeRecipe(savedRecipe.key)}}><i className="fas fa-heart" ></i></span>
-                    : <span onClick={() => { addRecipeFunction(recipeData)}}><i className="far fa-heart"></i></span>
+                isSaved
+                    ?
+                    <button className="heartButton" onClick={() => { removeRecipe(savedRecipe.key) }}>
+                        <i className="fas fa-heart" aria-hidden="true"></i>
+                        <span className="srOnly">Click to remove recipe remove saved recipe list</span>
+                    </button>
+                    :
+                    <button className="heartButton" onClick={() => { addRecipeFunction(recipeData) }}>
+                        <i className="far fa-heart" aria-hidden="true"></i>
+                        <span className="srOnly">Click to add recipe to saved recipe list</span>
+                    </button>
             }
 
             <h2>{foodName}</h2>
 
-            <img src={foodImg} alt="" onError={(event) => { event.target.src = selfie }}/>
+            <img src={foodImg} alt={`${foodName}`} onError={(event) => { event.target.src = notFoundPhoto; event.target.alt = "photo not found" }} />
             <p>{parseInt(calories)} Cal</p>
 
             <button onClick={handleShow}>Ingredient List</button>
@@ -46,6 +55,8 @@ const RecipeCard = ({ recipeData, addRecipeFunction, savedRecipes, removeRecipe}
             {show ? <RecipeDetails ingredientData={ingredientList} /> : ""}
 
             <a href={recipeSource} target="_blank" rel="noreferrer">Recipe link</a>
+
+            <button>Nutrition Facts</button>
 
         </div>
     )
--- src/components/SavedRecipeContainer.js
+++ src/components/SavedRecipeContainer.js
@@ -6,22 +6,32 @@ const SavedRecipeContainer = ({ recipeListData, removeRecipeFunction }) => {
 
     return (
         <>
-            <span onClick={() => {setShowList(!showList)}}><i className="fas fa-heart"></i></span>
-            
-            <ul className={showList ? "savedRecipeList toShow" : "savedRecipeList"}>
-                <h3>Your Recipe List</h3>
-                <p>{recipeListData.length} recipe{recipeListData.length > 1 ? "s" : ""} in list</p>
-                {
-                    recipeListData.map((recipe) => {
-                        return (
-                            <li key={recipe.key} className="recipeList">
-                                <p>{recipe.name}</p>
-                                <span onClick={() => { removeRecipeFunction(recipe.key) }}><i className="fas fa-trash-alt"></i></span>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+            <div className="savedListContainer">
+                <button className="heartButton fixedHeart" onClick={() => { setShowList(!showList) }}>
+                    <i className="fas fa-heart" aria-hidden="true"></i>
+                    <span className="srOnly">Click to open saved recipes</span>
+                </button>
+
+                <div className={showList ? "savedRecipeList toShow" : "savedRecipeList"}>
+                    <h3>Your Recipe List</h3>
+                    <p>{recipeListData.length} recipe{recipeListData.length > 1 ? "s" : ""} in list</p>
+                    <ul>
+                        {
+                            recipeListData.map((recipe) => {
+                                return (
+                                    <li key={recipe.key} className="recipeList">
+                                        <p>{recipe.name}</p>
+                                        <button onClick={() => { removeRecipeFunction(recipe.key) }}>
+                                            <i className="fas fa-trash-alt" aria-hidden="true"></i>
+                                            <span className="srOnly">click to delete the recipe</span>
+                                        </button>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }

8c3ea23ffb9348b4d256a67068b3630ba4634fbb
Meining Cheng <74748581+Meining2020@users.noreply.github.com>
2021-06-11 22:54:05 -0400

    Revert "Revert "merge new updates to production""
    This reverts commit f5343e573bd2e670cd260bf249ff3de527a1eebe.

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,18 +1,103 @@
-const RecipeDetails = ({ingredientData}) => {
+
+const RecipeDetails = (props) => {
+    console.log(props)
+    console.log(props.match.params)
+    console.log(props.location.state)
+    const { totalNutrients, totalWeight, totalDaily } = props.location.state;
+
     return (
-            <ul className="ingredients">
-                {
-                    //map through ingredient list to show each ingredient 
-                    ingredientData.map((ingredient, index) => {
-                        return (
-                            <li key={index}>{index + 1}. {ingredient}</li>
-                        )
-                    })
-                }
-            </ul>
+        <div className="tableContainer">
+            <div className="wrapper">
+                <table>
+                    <caption>Nutrition Facts</caption>
+
+                    <thead><th colspan="3">per {Math.round(totalWeight)}g</th></thead>
+
+                    <tbody>
+                        <tr>
+
+                            <th>Calories {Math.round(totalNutrients.ENERC_KCAL.quantity)}</th>
+                            <th>
+                                <a href="https://www.canada.ca/en/health-canada/services/understanding-food-labels/percent-daily-value.html" target="_blank" rel="noreferrer">% Daily Value</a>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td>Fat {Math.round(totalNutrients.FAT.quantity)} g</td>
+                            <td>{Math.round(totalDaily.FAT.quantity)} %</td>
+                        </tr>
+                        <tr>
+                            <td>Saturated {Math.round(totalNutrients.FASAT.quantity)} g</td>
+                            <td>{Math.round(totalDaily.FASAT.quantity)} %</td>
+                        </tr>
+                        {/* <tr>
+                    <td>+ Trans {totalNutrients.FATRN.quantity} g</td>
+                    <td></td>
+                </tr> */}
+                        <tr>
+                            <td>Carbohydrate {Math.round(totalNutrients.CHOCDF.quantity)} g</td>
+                            <td>{Math.round(totalDaily.CHOCDF.quantity)} %</td>
+                        </tr>
+                        <tr>
+                            <td>Fibre {Math.round(totalNutrients.FIBTG.quantity)} g</td>
+                            <td>{Math.round(totalDaily.FIBTG.quantity)} %</td>
+                        </tr>
+                        {/* <tr>
+                    <td>Sugar {totalNutrients.SUGAR.quantity} g</td>
+                    <td>{totalDaily.SUGAR.quantity} %</td>
+                </tr> */}
+                        <tr>
+                            <td>Protein {Math.round(totalNutrients.PROCNT.quantity)} g</td>
+                            <td>{Math.round(totalDaily.PROCNT.quantity)} %</td>
+                        </tr>
+                        <tr>
+                            <td>Cholesterol {Math.round(totalNutrients.CHOLE.quantity)} mg</td>
+                            <td>{Math.round(totalDaily.CHOLE.quantity)} %</td>
+                        </tr>
+                        <tr>
+                            <td>Sodium {Math.round(totalNutrients.NA.quantity)} mg</td>
+                            <td>{Math.round(totalDaily.NA.quantity)} %</td>
+                        </tr>
+                        <tr>
+                            <td>Potassium {Math.round(totalNutrients.K.quantity)} mg</td>
+                            <td>{Math.round(totalDaily.K.quantity)} %</td>
+                        </tr>
+                        <tr>
+                            <td>Calcium {Math.round(totalNutrients.CA.quantity)} mg</td>
+                            <td>{Math.round(totalDaily.CA.quantity)} %</td>
+                        </tr>
+                        <tr>
+                            <td>Iron {Math.round(totalNutrients.FE.quantity)} mg</td>
+                            <td>{Math.round(totalDaily.FE.quantity)} %</td>
+                        </tr>
+
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
     )
 }
 
+
 export default RecipeDetails;
 
+
+
+    // const {
+    //     ENERC_KCAL,
+    //     FAT,
+    //     FASAT,
+    //     FATRN,
+    //     CHOLE,
+    //     NA,
+    //     CHOCDF,
+    //     FIBTG,
+    //     SUGAR,
+    //     PROCNT,
+    //     K,
+    //     VITA_RAE,
+    //     VITC,
+    //     CA,
+    //     FE
+    // } = totalNutrients;
 
---
+++ src/components/SearchForm.js
@@ -0,0 +1,66 @@
+
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const SearchForm = () => {
+
+    const [userInput, setUserInput] = useState("");
+    // const [query, setQuery] = useState("");
+    const [noInput, setNoInput] = useState(false);
+
+
+    //function to get user's input
+    const handleUserInput = (event) => {
+        let inputValue = event.target.value;
+        setUserInput(inputValue);
+    }
+    
+    const handleSubmitClick = () => {
+        // event.preventDefault();
+        // setQuery(userInput);  
+        if (userInput) {
+            setNoInput(false);
+        } else {
+            setNoInput(true);
+        }
+
+        document.querySelector("main").scrollIntoView({ behavior: 'smooth' });
+    }
+
+    return (
+        <div className="formContainer">
+            <div className="wrapper">
+                {/* search area */}
+                <form className="searchForm">
+                    {/* label for screen readers only*/}
+                    <label htmlFor="input" className="srOnly">input keyword to search recipes</label>
+                    {/* search bar*/}
+                    <input type="text" id="input" value={userInput} onChange={handleUserInput} placeholder="Search food" />
+
+                    {/* submit button */}
+                    <Link to={`/results/${userInput}`}>
+                        <button type="submit" onClick={handleSubmitClick}>
+                            <i className="fas fa-search" aira-hidden="true"></i>
+                            <span className="srOnly">click to start search</span>
+
+                        </button>
+                    </Link>
+
+                </form>
+                {/* error message for no input */}
+                {
+
+                    noInput
+                        ? <p>Please enter a food!</p>
+                        : ""
+                }
+            </div>
+
+        </div>
+    )
+}
+
+
+export default SearchForm;
+
+
